0ad6d2b7b7f729c86ad45c56ac6dfa38f5d3a262
LuGMM <martinnezl@gmail.com>
2026-04-23 14:42:33 -0400

    first commit

---
+++ app/components/header.tsx
@@ -0,0 +1,39 @@
+import leftBracket from "@/public/Rectangle 2710.png";
+import rightBracket from "@/public/Rectangle 2711.png";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex flex-row h-[64px] w-full justify-between py-3 mb-3 relative z-1000">
+      <div className="flex flex-row pt-1 scale-75 justify-center items-center">
+        <Link
+          href={"/"}
+          className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md transition-colors h-9 px-4 py-2 font-semibold text-sm mr-2 line-clamp-4 leading-[16px] text-[#1A1B1C] z-1000"
+        >
+          SKINSTRIC
+        </Link>
+        <img
+          className="opacity-60 w-[5px] h-[17px]"
+          src={leftBracket.src}
+          width={5}
+          height={19}
+          loading="lazy"
+          alt="left bracket"
+        />
+        <p className="text-[#1a1b1c83] text-opacity-70  font-semibold text-sm ml-1.5 mr-1.5">
+          INTRO
+        </p>
+        <img
+          className="opacity-60 w-[5px] h-[17px]"
+          width={5}
+          height={19}
+          src={rightBracket.src}
+          alt="right bracket"
+        />
+      </div>
+      <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap font-semibold  transition-colors  disabled:pointer-events-none text-primary-foreground shadow h-9 px-4 py-2 mx-4 scale-[0.8] text-[#FCFCFC] text-[10px] bg-[#1A1B1C] ">
+        Enter Code
+      </button>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,97 @@
-import Image from "next/image";
+"use client";
+
+import Link from "next/link";
+import { useEffect } from "react";
+import { IoIosPlay } from "react-icons/io";
 
 export default function Home() {
+  useEffect(() => {
+    const left = document?.getElementById("left");
+    const right = document?.getElementById("right");
+
+    left?.addEventListener("mouseenter", () => {
+      console.log("enter");
+    });
+    left?.addEventListener("mouseleave", () => {
+      console.log("leave");
+    });
+    right?.addEventListener("mouseenter", () => {
+      console.log("enter");
+    });
+    right?.addEventListener("mouseleave", () => {
+      console.log("leave");
+    });
+  }, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div>
+      <div className="max-sm:scale-[0.75] max-sm:origin-center max-sm:p-6">
+        <div className="flex flex-col items-center justify-center h-[71dvh] md:fixed md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
+          <div className="absolute inset-0 flex items-center justify-center lg:hidden">
+            <div className="w-[350px] h-[350px] border border-dotted border-[#A0A4AB] rotate-45 absolute top-1/2 left-1/2 -translate-x-[52%] -translate-y-1/2"></div>
+          </div>
+          <div className="absolute inset-0 flex items-center justify-center lg:hidden">
+            <div className="w-[420px] h-[420px] border border-dotted border-[#A0A4AB] rotate-45 absolute top-1/2 left-1/2 -translate-x-[52%] -translate-y-1/2"></div>
+          </div>
+          <div className="relative z-10 text-center">
+            <h1 className="text-[60px] text-[#1A1B1C] lg:text-[100px] font-inter font-normal tracking-tighter leading-none">
+              Sophisticated <br /> Skincare
+            </h1>
+          </div>
+          <p className="z-10 block lg:hidden w-[30ch] mt-4 text-[16px] font-semibold text-center text-muted-foreground text-[#1a1b1c83]">
+            Skinstric developed an A.I. that creates a highly-personalized
+            routine tailored to what your skin needs.
           </p>
+          <div>
+            <Link className="z-10 mt-4 lg:hidden" href="/">
+              <div className="relative flex items-center gap-4 hover:scale-105 duration-300 font-bold text-[12px]">
+                ENTER EXPERIENCE
+                <div className="relative flex items-center justify-center w-[24px] h-[24px] border border-solid border-black rotate-315">
+                  <IoIosPlay className=" scale-[0.75] hover:scale-100 duration-300 rotate-45 absolute bottom-1 right-1" />
+                </div>
+              </div>
+            </Link>
+            <div className="hidden lg:block fixed bottom-[calc(-7vh)] left-[calc(-20vw)] xl:left-[calc(-27vw)] 2xl:left-[calc(-31vw)] [@media(width>=1920px)]:left-[calc(-33vw)] font-normal text-sm text-[#1A1B1C] space-y-3 ">
+              <p>
+                SKINSTRIC DEVELOPED AN A.I. THAT CREATES A
+                <br />
+                HIGHLY-PERSONALIZED ROUTINE TAILORED TO
+                <br />
+                WHAT YOUR SKIN NEEDS.
+              </p>
+            </div>
+            <div className="hidden lg:block fixed left-[calc(-53vw)] xl:left-[calc(-50vw)] top-1/2 -translate-y-1/2 w-[500px] h-[500px] transition-opacity duration-500 ease-in-out opacity-100">
+              <div className="relative w-full h-full">
+                <div className="w-full h-full border border-dotted border-[#A0A4AB] rotate-45 fixed inset-0"></div>
+                <div
+                  id="left"
+                  className="group inline-flex items-center justify-center gap-4 whitespace-nowrap rounded-md text-sm font-normal text-[#1A1B1C] transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring cursor-not-allowed disabled:opacity-50 h-9 absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/5 xl:translate-x-1/6 [@media(width>=1920px)]:translate-x-1/20 px-3 py-1 "
+                >
+                  <div className="w-[30px] h-[30px] border border-solid border-black rotate-45  group-hover:scale-110 duration-300 relative">
+                    <IoIosPlay className=" scale-100 hover:scale-160 duration-300 rotate-135 absolute bottom-1 left-1" />
+                  </div>
+                  DISCOVER A.I.
+                </div>
+              </div>
+            </div>
+            <div className="hidden lg:block fixed top-1/2 right-[calc(-53vw)] xl:right-[calc(-50vw)] -translate-y-1/2 w-[500px] h-[500px] transition-opacity duration-500 ease-in-out opacity-100">
+              <div className="relative w-full h-full">
+                <div className="w-full h-full border border-dotted border-[#A0A4AB] rotate-45 absolute inset-0"></div>
+                <Link
+                  id="right"
+                  href="/testing"
+                  className="group inline-flex items-center justify-center gap-4 whitespace-nowrap rounded-md text-sm font-normal text-[#1A1B1C] transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring cursor-pointer disabled:opacity-50 h-9 absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/5 xl:-translate-x-1/6 [@media(width>=1920px)]:-translate-x-1/20 px-3 py-1"
+                >
+                  TAKE TEST
+                  <div className="w-[30px] h-[30px] border border-solid border-black rotate-45 group-hover:scale-110 duration-300 relative">
+                    <IoIosPlay className=" scale-100 hover:scale-130 duration-300 rotate-315 absolute top-1 right-1" />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
